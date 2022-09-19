131b2b4c9f0375101be1dbba620084fbceb93dd4
arinzejustin <arinzejustinng@gmail.com>
2022-09-19 11:01:57 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   chainWebpack: config => config.resolve.symlinks(false)
-})
+}
